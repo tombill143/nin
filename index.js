5b9431af3c181aa9e210ba2038dc554495c76c2c
tom <tom@twentyfour.dk>
2023-12-12 12:49:35 +0100

    working on navigation

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 window.addEventListener("load",setup);
 
 function setup(){
+    setupBurgerNav();
+    
+    //toggle accordion
     const h2s = document.querySelectorAll(".discography,main,h2");
     if(h2s){
         h2s.forEach(h2 => [
@@ -10,5 +13,14 @@ function setup(){
             })
         ])
     }
+    
+}
 
+function setupBurgerNav(){
+    const burger = document.querySelector("header svg");
+    const nav = document.querySelector("nav");
+    burger.addEventListener('click', e=> {
+        burger.classList.toggle("open");
+        nav.classList.toggle("open");
+    });
 }
